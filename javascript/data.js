bd57c40a6256694d0b272621a1fb0e0e622823f5
Martin <martinlicciardello7@gmail.com>
2021-07-21 20:42:55 -0300

    Casa Venecia 3.0

---
+++ javascript/data.js
@@ -0,0 +1,119 @@
+//  SECCIÓN DE DECLARACIÓN DE INFORMACIÓN ESTÁTICA
+let DATOSCERAMICAS = [{
+    "nombre" : "Alamo",
+    "marca" : "Cortines",
+    "precio" : 2000,
+    "imagen" : "./ImagenesPag/ceramicaalamo.PNG",
+    "id" : 1
+  },{
+    "nombre" : "Castaño",
+    "marca" :"Cortines",
+    "precio" : 2000,
+    "imagen" : "./ImagenesPag/ceramicacastaño.PNG",
+    "id" : 2
+  
+  },{
+    "nombre" : "Calipso",
+    "marca" : "Cortines",
+    "precio" : 2200,
+    "imagen" : "./ImagenesPag/ceramicacalipso.png",
+    "id" : 3
+  
+  },{
+    "nombre" : "Pizarra",
+    "marca" : "Cortines",
+    "precio" : 2400,
+    "imagen" : "./ImagenesPag/ceramicapizzara.PNG",
+    "id" : 4
+  
+  },{
+    "nombre" : "Parquet",
+    "marca" : "Allpa",
+    "precio" : 2200,
+    "imagen" : "./ImagenesPag/ceramicaparquet.jpg",
+    "id" : 5
+  
+  },{
+    "nombre" : "Garden",
+    "marca" : "Cortines",
+    "precio" : 2400,
+    "imagen" : "./ImagenesPag/ceramicagarden.jpg",
+    "id" : 6
+  },{
+    "nombre" : "Coral",
+    "marca" : "Cortines",
+    "precio" : 2400,
+    "imagen" : "./ImagenesPag/ceramicacoral.PNG",
+    "id" : 7
+  
+  },{
+    "nombre" : "Modena",
+    "marca" : "Allpa",
+    "precio" : 2500,
+    "imagen" : "./imagenesPag/ceramicamodena.jpg",
+    "id" : 8
+  
+  },{
+    "nombre" : "Morisco",
+    "marca" : "Allpa",
+    "precio" : 2500,
+    "imagen" : "./ImagenesPag/ceramicacoral.PNG",
+    "id" : 9
+  
+  },{
+    "nombre" : "Lenga",
+    "marca" : "Cortines",
+    "precio" : 2200,
+    "imagen" : "./imagenesPag/ceramicalenga.jpg",
+    "id" : 10
+},{
+    "nombre" : "Neon",
+    "marca" : "Cortines",
+    "precio" : 2300,
+    "imagen" : "./imagenesPag/ceramicaneon.PNG",
+    "id" : 11
+},{
+    "nombre" : "Venecita Azul",
+    "marca" : "Cortines",
+    "precio" : 2100,
+    "imagen" : "./imagenesPag/ceramicavenecitaazul.PNG",
+    "id" : 12
+},{
+    "nombre" : "Venecita Mix",
+    "marca" : "Cortines",
+    "precio" : 2100,
+    "imagen" : "./imagenesPag/ceramicavenecitamix.PNG",
+    "id" : 13
+},{
+    "nombre" : "Travertino",
+    "marca" : "Cortines",
+    "precio" : 2300,
+    "imagen" : "./imagenesPag/ceramicatravertino.PNG",
+    "id" : 14
+  },{
+    "nombre" : "Madera parquet",
+    "marca" : "Lourdes",
+    "precio" : 2500,
+    "imagen" : "./imagenesPag/ceramicalourdesmadera.PNG",
+    "id" : 15
+  },{
+    "nombre" : "Negra Plus",
+    "marca" : "Lourdes",
+    "precio" : 2300,
+    "imagen" : "./imagenesPag/ceramicalourdesnegraplus.PNG",
+    "id" : 16
+  },{
+    "nombre" : "Pino claro",
+    "marca" : "Lourdes",
+    "precio" : 2500,
+    "imagen" : "./imagenesPag/ceramicalourdespinoclaro.PNG",
+    "id" : 17
+  },{
+    "nombre" : "San Telmo",
+    "marca" : "Lourdes",
+    "precio" : 2500,
+    "imagen" : "./imagenesPag/ceramicalourdessantelmo.PNG",
+    "id" : 18
+  }]
+
+  let SELECCIONADOS = [];
